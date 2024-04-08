d85f9ba396792370f27f251fbf500869d2908886
Aleks Todorov <aleks.todorov.1337@gmail.com>
2024-04-08 13:30:20 +0100

    Add a reminder to manually install plugins

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -2,4 +2,4 @@
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s "$(realpath "$(dirname "$0")/.tmux.conf")" ~/.tmux.conf
-tmux source ~/.tmux.conf
+tmux source ~/.tmux.conf # Remember to use <prefix>+Shift+i to install plugins
