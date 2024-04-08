74a3c47fd86e2f679daee70334979111093c16ab
Aleks Todorov <aleks.todorov.1337@gmail.com>
2024-04-08 13:27:31 +0100

    Symbolically link tmux config instead of copying
    Now the config will use the source repo as the single source of truth,
    so that changes can be made and committed.

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-cp -f .tmux.conf ~/.tmux.conf
+ln -s "$(realpath "$(dirname "$0")/.tmux.conf")" ~/.tmux.conf
 tmux source ~/.tmux.conf
