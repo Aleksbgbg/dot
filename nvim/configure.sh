95d382c26134ef456ce9343f48756ee184fde06f
Aleks Todorov <aleks.todorov.1337@gmail.com>
2024-04-16 16:10:15 +0100

    nvim: Copy and use vim config

--- nvim/configure.sh
+++ nvim/configure.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# Config
+mkdir -p ~/.config/nvim
+ln -s "$(realpath "$(dirname "$0")/init.vim")" ~/.config/nvim/init.vim
+
+# Treesitter
 git clone https://github.com/nvim-treesitter/nvim-treesitter ~/.local/share/nvim/site/pack/treesitter/start/nvim-treesitter
---
+++ nvim/init.vim
@@ -0,0 +1,9 @@
+" Hybrid line numbers
+set number relativenumber
+set nu rnu
+
+" Tabs -> spaces
+filetype plugin indent on
+set tabstop=2
+set shiftwidth=2
+set expandtab
