06b974565fbb413ea18ebcdb24b762eb5f4991f3
Aleks Todorov <aleks.todorov.1337@gmail.com>
2024-04-19 11:11:51 +0100

    nvim: Use Lazy to install nvim-treesitter

--- nvim/configure.sh
+++ nvim/configure.sh
@@ -4,6 +4,3 @@
 mkdir -p ~/.config/nvim/lua
 ln -s "$(realpath "$(dirname "$0")/init.vim")" ~/.config/nvim/init.vim
 ln -s "$(realpath "$(dirname "$0")/init.lua")" ~/.config/nvim/lua/init.lua
-
-# Treesitter
-git clone https://github.com/nvim-treesitter/nvim-treesitter ~/.local/share/nvim/site/pack/treesitter/start/nvim-treesitter
--- nvim/init.lua
+++ nvim/init.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 })
 
 -- Catpuccin colour scheme
