e2a1855718cb1ff3d3f264a3229e7123b326e5f8
Aleks Todorov <aleks.todorov.1337@gmail.com>
2024-05-10 12:34:11 +0000

    nvim: Add TS configuration for blueprint files
    Include a script to clone Bruno's tree-sitter-bp repo.

--- nvim/init.lua
+++ nvim/init.lua
@@ -67,3 +67,17 @@ vim.filetype.add({
 
 -- Nvim Tree config
 require("nvim-tree").setup()
+
+-- Treesitter Android.bp plugin
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.bp = {
+  install_info = {
+    url = "~/dev/tree-sitter-bp", -- local path or git repo
+    files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
+    -- optional entries:
+    branch = "main", -- default branch in case of git repo if different from master
+    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+  },
+  filetype = "bp", -- if filetype does not match the parser name
+}
