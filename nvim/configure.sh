ece827d34f2fc55716eb2b2490e7ee68ccd93d76
Aleks Todorov <aleks.todorov.1337@gmail.com>
2024-05-10 12:34:11 +0000

    nvim: Add TS configuration for blueprint files
    Include a script to clone Bruno's tree-sitter-bp repo.

--- nvim/configure.sh
+++ nvim/configure.sh
@@ -4,3 +4,12 @@
 mkdir -p ~/.config/nvim/lua
 ln -s -f "$(dirname "$(realpath "$0")")/init.vim" ~/.config/nvim/init.vim
 ln -s -f "$(dirname "$(realpath "$0")")/init.lua" ~/.config/nvim/lua/init.lua
+
+# TreeSitter bp grammars
+git clone https://git.belanyi.fr/ambroisie/tree-sitter-bp ~/dev/tree-sitter-bp
+ln -s ~/dev/tree-sitter-bp/queries ~/.local/share/nvim/lazy/nvim-treesitter/queries/bp
+
+pushd ~/dev/tree-sitter-bp
+cargo install tree-sitter-cli
+tree-sitter generate
+popd
