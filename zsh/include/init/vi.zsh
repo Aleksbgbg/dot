cf6bebed4b7c0ebea855e9b24a01ee7b6c2e4b46
Aleks Todorov <aleks.todorov.1337@gmail.com>
2024-10-18 11:26:24 +0100

    zsh: Migrate local_history.zsh to vi.zsh
    Now that we've migrated to vi mode, ensure it uses local history.
    
    Note that this is incomplete, and for some reason up and down arrows
    still use global history, even though vi keys navigate as intended.

--- zsh/include/init/vi.zsh
+++ zsh/include/init/vi.zsh
@@ -1,6 +1,28 @@
+() {
+  function vi-up-line-or-local-history() {
+      zle set-local-history 1
+      zle vi-up-line-or-history
+      zle set-local-history 0
+  }
+  zle -N vi-up-line-or-local-history
+
+  function vi-down-line-or-local-history() {
+      zle set-local-history 1
+      zle vi-down-line-or-history
+      zle set-local-history 0
+  }
+  zle -N vi-down-line-or-local-history
+
+  down_key=${terminfo[kcud1]}
+  bindkey -M viins "${down_key}" vi-down-line-or-local-history
+
+  up_key=${terminfo[kcuu1]}
+  bindkey -M viins "${up_key}" vi-up-line-or-local-history
+}
+
 bindkey -M vicmd j vi-backward-char
-bindkey -M vicmd k vi-down-line-or-history
-bindkey -M vicmd l vi-up-line-or-history
+bindkey -M vicmd k vi-down-line-or-local-history
+bindkey -M vicmd l vi-up-line-or-local-history
 bindkey -M vicmd \; vi-forward-char
 
 bindkey -M visual j vi-backward-char
