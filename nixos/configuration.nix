8f7863a170bd14c6032efc4ddb044362a3ae59cc
Aleks Todorov <aleks.todorov.1337@gmail.com>
2025-05-31 19:50:34 +0100

    nixos: Format nix files with alejandra

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,14 +1,15 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   # Bootloader.
   boot.loader.grub.enable = true;
@@ -86,12 +87,12 @@
   users.users.aleksbgbg = {
     isNormalUser = true;
     description = "Aleks Todorov";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = ["networkmanager" "wheel"];
     packages = with pkgs; [
       kdePackages.kate
       git
       alejandra
-    #  thunderbird
+      #  thunderbird
     ];
   };
 
@@ -106,8 +107,8 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #  wget
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -136,5 +137,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
-
 }
