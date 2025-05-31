0d893113024d5b2c0de70b0a8f1305efc208b7ed
Aleks Todorov <aleks.todorov.1337@gmail.com>
2025-05-31 21:06:51 +0100

    nixos: Add alacritty terminal

--- nixos/home.nix
+++ nixos/home.nix
@@ -27,5 +27,21 @@
   };
   home.file.".config/nvim/lua/init.lua".text = lib.fileContents ../nvim/init.lua;
 
+  programs.alacritty = {
+    enable = true;
+
+    settings = {
+      env.TERM = "xterm-256color";
+
+      font = {
+        size = 10;
+      };
+
+      scrolling.multiplier = 5;
+
+      selection.save_to_clipboard = true;
+    };
+  };
+
   home.stateVersion = "25.05";
 }
