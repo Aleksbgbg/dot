d46e76768fb2bd63540853b5b099568ba0edff35
Aleks Todorov <aleks.todorov.1337@gmail.com>
2025-05-31 20:58:26 +0100

    nixos: Add C and C++ compiler (clang)

--- nixos/systems/aleksbgbg-d/configuration.nix
+++ nixos/systems/aleksbgbg-d/configuration.nix
@@ -108,6 +108,7 @@
   environment.systemPackages = with pkgs; [
     git
     vim
+    llvmPackages_20.clangWithLibcAndBasicRtAndLibcxx
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
