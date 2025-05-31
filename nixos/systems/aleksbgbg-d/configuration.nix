444da4d84379bcf41921e1bb8593e276a9979d1b
Aleks Todorov <aleks.todorov.1337@gmail.com>
2025-05-31 21:27:16 +0100

    nixos: Add NodeJs

--- nixos/systems/aleksbgbg-d/configuration.nix
+++ nixos/systems/aleksbgbg-d/configuration.nix
@@ -114,6 +114,10 @@
     cargo
     rust-analyzer
     cargo-watch
+
+    # Web development
+    nodejs_24
+
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
