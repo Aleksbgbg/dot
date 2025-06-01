b95bd37dfcea1c5bbf9fe05e018564979e1d0432
Aleks Todorov <aleks.todorov.1337@gmail.com>
2025-06-01 22:10:39 +0100

    nixos: Add git-revise

--- nixos/systems/aleksbgbg-d/configuration.nix
+++ nixos/systems/aleksbgbg-d/configuration.nix
@@ -123,6 +123,9 @@
     # Web development
     nodejs_24
 
+    # Tools
+    git-revise
+
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
