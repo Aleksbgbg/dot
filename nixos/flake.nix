b1471bdcbfb297f09180b57ef2a9fd301ced81b7
Aleks Todorov <aleks.todorov.1337@gmail.com>
2025-06-02 18:32:03 +0100

    nixos: Switch home-manager input syntax
    I prefer this syntax for inputs specifically as it's more linear and
    easier to follow.

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -4,10 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
