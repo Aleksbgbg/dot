4dafba12d1e1bc570ef7d3d344dd4213363d1fd2
Aleks Todorov <aleks.todorov.1337@gmail.com>
2025-06-04 20:49:28 +0100

    nixos: Move git config to home module

--- nixos/systems/aleksbgbg-d/home.nix
+++ nixos/systems/aleksbgbg-d/home.nix
@@ -3,59 +3,12 @@
   lib,
   ...
 }: {
+  imports = [
+    ../../home/programs/git.nix
+  ];
+
   home.username = "aleksbgbg";
   home.homeDirectory = "/home/aleksbgbg";
-
-  programs.git = {
-    enable = true;
-
-    userName = "Aleks Todorov";
-    userEmail = "aleks.todorov.1337@gmail.com";
-
-    aliases = {
-      ame = "commit --amend --no-edit";
-      amed = "commit --amend";
-      bla = "blame -w -C -C -C";
-      chp = "cherry-pick";
-      com = "commit";
-      coms = "commit --sign";
-      dif = "diff --staged";
-      ff = "merge --ff-only";
-      fix = "commit --fixup";
-      lo = "log --oneline";
-      lol = "log --graph --pretty=format:'%C(auto)%h%d%Creset %C(cyan)(%cr)%Creset %C(green)%cn <%ce>%Creset %s'";
-      lold = "log --graph --pretty=format:'%C(auto)%h%d%Creset %C(cyan)(%ci)%Creset %C(green)%cn <%ce>%Creset %s'";
-      los = "log --format=%<(50,trunc)%s";
-      pullr = "pull --rebase";
-      pushf = "push --force-with-lease";
-      rb = "rebase";
-      sta = "stash save --staged --message";
-      sw = "switch";
-      swc = "switch --create";
-    };
-
-    extraConfig = {
-      color.ui = "auto";
-
-      commit.verbose = true;
-
-      core.autocrlf = "input";
-      core.editor = "nvim";
-      core.ignorecase = false;
-
-      diff.algorithm = "patience";
-
-      gc.pruneexpire = "90 days";
-
-      merge.conflictStyle = "diff3";
-
-      pull.ff = "only";
-
-      rebase.autostash = true;
-
-      rerere.enabled = true;
-    };
-  };
 
   programs.neovim = {
     enable = true;
