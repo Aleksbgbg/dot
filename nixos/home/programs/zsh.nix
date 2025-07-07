00116533cb754d0563c94c97ca0e73b1c795e6ec
aleksbgbg <aleks.todorov.1337@gmail.com>
2025-07-07 22:43:50 +0100

    nixos: zsh: Add zsh-vi-mode
    Add zsh-vi-mode plugin for vi-style editing on the command line.
    
    Include a custom startup script to switch the keybindings to my
    preferred jkl; layout instead of hjkl.
    
    Unfortunately, I also used my own fork of zsh-vi-mode to remove the
    history keybindings, as this kept overriding fzf's keybindings and I
    couldn't find a way to restore the fzf behaviour otherwise - if I find a
    way to do so, I can revert back to the original project then.

--- nixos/home/programs/zsh.nix
+++ nixos/home/programs/zsh.nix
@@ -1,7 +1,24 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   programs.zsh = {
     enable = true;
 
     history.size = 50000;
+
+    plugins = [
+      {
+        name = "zsh-vi-mode";
+        file = "zsh-vi-mode.plugin.zsh";
+        src = fetchGit {
+          url = "https://github.com/Aleksbgbg/zsh-vi-mode";
+          rev = "e3fb2715161440a1c37df29169530cad3615600a";
+        };
+      }
+    ];
+
+    initContent = lib.fileContents ./vi.zsh;
   };
 }
