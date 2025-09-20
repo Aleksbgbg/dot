6b776af5dffddd26230c7489cfbb226485d23b89
Aleks Todorov <aleks.todorov.1337@gmail.com>
2025-09-20 20:05:38 +0100

    nixos: flutter: Fix clang linking issue
    When installing flutter, clang becomes unable to link with `-lc++`
    unless clang is installed again after flutter. Strange. Nevertheless,
    fix the issue by installing clang twice, for now.

--- nixos/modules/flutter-dev.nix
+++ nixos/modules/flutter-dev.nix
@@ -1,5 +1,9 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     flutter
+
+    # Should already be installed, but after flutter is installed, clang can't
+    # link with `-lc++` without this...?
+    llvmPackages.clang
   ];
 }
