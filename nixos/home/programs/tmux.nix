ae6884b2689a9bf7718f1e3856190c3912bc2f04
Aleks Todorov <aleks.todorov.1337@gmail.com>
2025-09-20 22:43:34 +0100

    nixos: tmux: Add tmux-resurrect
    Allow saving the workspace contents and reloading them later.
    
    Config sourced from [1].
    
    [1] https://discourse.nixos.org/t/how-to-get-tmux-resurrect-to-restore-neovim-sessions/30819/7?u=aleksbgbg

--- nixos/home/programs/tmux.nix
+++ nixos/home/programs/tmux.nix
@@ -8,6 +8,17 @@
 
     plugins = with pkgs.tmuxPlugins; [
       nord
+
+      {
+        plugin = resurrect;
+        extraConfig = ''
+          resurrect_dir="$HOME/.tmux/resurrect"
+          set -g @resurrect-dir $resurrect_dir
+          set -g @resurrect-capture-pane-contents 'on'
+          set -g @resurrect-hook-post-save-all "sed 's/--cmd[^ ]* [^ ]* [^ ]*//g' $resurrect_dir/last | sponge $resurrect_dir/last"
+          set -g @resurrect-processes '"~nvim"'
+        '';
+      }
     ];
 
     extraConfig = lib.fileContents ../../../tmux/.tmux.conf;
