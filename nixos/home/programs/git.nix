c6b3f695ae49f52d5361fe6d2a62f7387b071f84
Aleks Todorov <aleks.todorov.1337@gmail.com>
2025-12-28 19:32:05 +0000

    nixos: Update deps to latest versions for Q1 2026
    The update for Opencraft, specifically, improves performance and fixes
    the green screen issue on many platforms.

--- nixos/home/programs/git.nix
+++ nixos/home/programs/git.nix
@@ -2,32 +2,34 @@
   programs.git = {
     enable = true;
 
-    userName = "Aleks Todorov";
-    userEmail = "aleks.todorov.1337@gmail.com";
+    settings = {
+      user = {
+        name = "Aleks Todorov";
+        email = "aleks.todorov.1337@gmail.com";
+      };
 
-    aliases = {
-      ame = "commit --amend --no-edit";
-      amed = "commit --amend";
-      bla = "blame -w -C -C -C";
-      chp = "cherry-pick";
-      com = "commit";
-      coms = "commit --sign";
-      dif = "diff --staged";
-      ff = "merge --ff-only";
-      fix = "commit --fixup";
-      lo = "log --oneline";
-      lol = "log --graph --pretty=format:'%C(auto)%h%d%Creset %C(cyan)(%cr)%Creset %C(green)%cn <%ce>%Creset %s'";
-      lold = "log --graph --pretty=format:'%C(auto)%h%d%Creset %C(cyan)(%ci)%Creset %C(green)%cn <%ce>%Creset %s'";
-      los = "log --format=%<(50,trunc)%s";
-      pullr = "pull --rebase";
-      pushf = "push --force-with-lease";
-      rb = "rebase";
-      sta = "stash save --staged --message";
-      sw = "switch";
-      swc = "switch --create";
-    };
+      alias = {
+        ame = "commit --amend --no-edit";
+        amed = "commit --amend";
+        bla = "blame -w -C -C -C";
+        chp = "cherry-pick";
+        com = "commit";
+        coms = "commit --sign";
+        dif = "diff --staged";
+        ff = "merge --ff-only";
+        fix = "commit --fixup";
+        lo = "log --oneline";
+        lol = "log --graph --pretty=format:'%C(auto)%h%d%Creset %C(cyan)(%cr)%Creset %C(green)%cn <%ce>%Creset %s'";
+        lold = "log --graph --pretty=format:'%C(auto)%h%d%Creset %C(cyan)(%ci)%Creset %C(green)%cn <%ce>%Creset %s'";
+        los = "log --format=%<(50,trunc)%s";
+        pullr = "pull --rebase";
+        pushf = "push --force-with-lease";
+        rb = "rebase";
+        sta = "stash save --staged --message";
+        sw = "switch";
+        swc = "switch --create";
+      };
 
-    extraConfig = {
       color.ui = "auto";
 
       commit.verbose = true;
